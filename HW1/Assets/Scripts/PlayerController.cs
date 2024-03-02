a463ef5103d39a2fdfc4f456bb258cd20141de59
ar <90264086+ahmadrezasam@users.noreply.github.com>
2024-03-03 00:01:43 +0300

    2.11 Making Objects Respond to Inputs: The player object (capsule) can be controlled using the keyboard's WASD commands. Additionally, it can ascend or jump using the space key. The values for Move Speed and Jump Force should be defined within the Inspector panel in the Player Controller script; the default value is 0.

--- HW1/Assets/Scripts/PlayerController.cs
+++ HW1/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed;
+    public float jumpForce;
+    public Rigidbody theRB;
+
+    // Use this for initialization
     void Start()
     {
-        
+        theRB = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
-        
+        theRB.velocity = new Vector3(Input.GetAxis("Horizontal")* moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);      
+        }
     }
 }
